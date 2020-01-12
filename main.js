2ea5d0945d67690cea62d2469c5163736444d557
Aloui-khalil <56514173+Aloui-khalil@users.noreply.github.com>
2020-01-12 16:14:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,296 @@
+
+function AddToCart() {
+   
+ if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart.png'){
+ document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart1.png';
+ swal( "Successfully added to cart!","", "success");
+ document.getElementById('EmptyCart').style.display= "none";
+ 
+ }else if(document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart1.png'){
+    document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart2.png';
+    swal( "Successfully added to cart!","", "success");
+    document.getElementById('EmptyCart').style.display= "none";
+
+ }else if(document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart2.png'){
+    document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart3.png';
+    swal( "Successfully added to cart!","", "success");
+    document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart3.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart4.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart4.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart5.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart5.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart6.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart6.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart7.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart7.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart8.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart8.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart9.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart9.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart10.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart10.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart11.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart11.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart12.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ }
+ else if (document.getElementById('Cart').src=='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart12.png') {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart13.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+ } else {
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart13.png';
+   swal( "Successfully added to cart!","", "success");
+   document.getElementById('EmptyCart').style.display= "none";
+
+ }
+ 
+}
+
+function GoCard1() {
+   document.getElementById('Coat1').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt1() {
+   document.getElementById('Coat1').style.display= "none";
+}
+
+
+function GoCard2() {
+   document.getElementById('Coat2').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt2() {
+   document.getElementById('Coat2').style.display= "none";
+}
+
+
+function GoCard3() {
+   document.getElementById('Coat3').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt3() {
+   document.getElementById('Coat3').style.display= "none";
+}
+
+
+function GoCard4() {
+   document.getElementById('Shirt1').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt4() {
+   document.getElementById('Shirt1').style.display= "none";
+}
+
+
+function GoCard5() {
+   document.getElementById('Shirt2').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt5() {
+   document.getElementById('Shirt2').style.display= "none";
+}
+
+
+function GoCard6() {
+   document.getElementById('Shirt3').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt6() {
+   document.getElementById('Shirt3').style.display= "none";
+}
+
+function GoCard7() {
+   document.getElementById('Shirt4').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt7() {
+   document.getElementById('Shirt4').style.display= "none";
+}
+
+function GoCard8() {
+   document.getElementById('Shirt5').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt8() {
+   document.getElementById('Shirt5').style.display= "none";
+}
+
+function GoCard9() {
+   document.getElementById('Pant1').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt9() {
+   document.getElementById('Pant1').style.display= "none";
+
+}
+
+function GoCard10() {
+   document.getElementById('Pant2').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+
+function Dlt10() {
+   document.getElementById('Pant2').style.display= "none";
+}
+
+function GoCard11() {
+   document.getElementById('Pant3').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt11() {
+   document.getElementById('Pant3').style.display= "none";
+}
+
+function GoCard12() {
+   document.getElementById('Shoes1').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt12() {
+   document.getElementById('Shoes1').style.display= "none";
+}
+
+function GoCard13() {
+   document.getElementById('Shoes2').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+function Dlt13() {
+   document.getElementById('Shoes2').style.display= "none";
+}
+
+
+function GoCard14() {
+   document.getElementById('Shoes3').style.display= "flex";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+   
+}
+
+
+function Dlt14() {
+   document.getElementById('Shoes3').style.display= "none";
+}
+
+
+
+
+function DltAll() {
+   document.getElementById('FullCart').style.display= "none";
+   document.getElementById('EmptyCart').style.display= "inline";
+   document.getElementById('DeleteAllButton').style.display= "none";
+   document.getElementById('Cart').src='file:///C:/Users/User/Desktop/GoMyCode/Proojet/Images/Cart.png';
+   console.log("Hope you find something else !! ");
+}
+
+
+function Gc1 () {
+   document.getElementById('GiftCard1').style.display= "inline";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+}
+
+function DltGc1 () {
+   document.getElementById('GiftCard1').style.display= "none";
+}
+
+function Gc2 () {
+   document.getElementById('GiftCard2').style.display= "inline";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+}
+
+
+function DltGc2 () {
+   document.getElementById('GiftCard2').style.display= "none";
+}
+
+function Gc3 () {
+   document.getElementById('GiftCard3').style.display= "inline";
+   document.getElementById('DeleteAndBuy').style.display= "inline";
+}
+
+function DltGc3 () {
+   document.getElementById('GiftCard3').style.display= "none";
+}
+
+
+
+   //Sign Up
+
+function Submit() {
+   var name = document.getElementById("InputName").value;
+   var email = document.getElementById("InputEmail").value;
+   var password = document.getElementById("InputPassword").value;
+   var password2 = document.getElementById("InputPassword2").value;
+
+   if (name == '' || email == '' || password == '' ){
+      swal({
+         title: "You forgot something !",
+         text: "Please fill in the empty boxes.",
+         icon: "warning",
+       });
+   } else  if (password !== password2){
+      swal({
+         title: "Please verify your password !",
+   
+         icon: "warning",
+       });
+   }
+   else{
+      swal({
+         title: "Successfully submitted !",
+         text: "Thanks for submitting "+name,
+         icon: "success",
+       });
+   }
+}
